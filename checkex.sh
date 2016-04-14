5a5791e77fb4e2dd22bc9e677cc9b22953b11a86
BorisZaydlin <boriszaydlin@gmail.com>
2016-04-14 10:29:48 +0000

    Removing echos

--- checkex.sh
+++ checkex.sh
@@ -1,139 +1,139 @@
 #!/usr/bin/env bash
-echo "=========================="
-echo "  Checking for exploits"
-echo "=========================="
+#echo "=========================="
+#echo "  Checking for exploits"
+#echo "=========================="
 echo "Subject: Exploits scan" >> /tmp/exploits.log
-echo "===="
+#echo "===="
 
 grep -rwn '$xnedsfj' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'x0uQrgyZmKs80Wipw' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 
 
 grep -rwn 'd730ca2c' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'maad91c31' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn '$mdwhp' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn '$ziwg' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn '$jawhtmkz' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn '10lbcZ8vGZ2MuwifLsWwI' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••••••••"
 
 grep -rwn '$mjagirjhf' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn '$uopjierpm' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••••••"
 
 grep -rwn 'rondxlinmzcox' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'cpuE2spWF' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'porus_et' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'qbd76ac' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••••"
 
 grep -rwn 'f@ck?' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'O7ddnZ++7H+' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 's3ib' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'Priv8' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'smtp_ssl' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'We have an error in card information' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'joining the millions' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'w4l3XzY3' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••••"
 grep -rwn 'BlesseD' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••••"
 grep -rwn 'some data from your account information seems' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••••"
 grep -rwn 'Because we think that your account' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••••"
 grep -rwn 'ncountered a problem in your account' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••••"
 grep -rwn 'TC9A16C47DA8EEE87' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••••"
 grep -rwn 'GX8MEGRA4UB4bWV0aG9kPSdwbwAA' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••••"
 grep -rwn 'ebscr=$errors2' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••••"
 grep -rwn 'LOFIN' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••••"
 grep -rwn 'leafTrim' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••••"
 grep -rwn 'randomletters' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••••"
 grep -rwn 'LyogS2FsYWNobmlrb' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••••"
 grep -rwn 'sYWNlKCJcXCIsICIvIiwg' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••••"
 grep -rwn 'Yassinox' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••••"
 grep -rwn 'fourilist' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••••"
+#echo "••••••••••••••••••••••••"
 grep -rwn 'joexploits' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••••"
+#echo "•••••••••••••••••••••••"
 grep -rwn 'allisonschemist' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••••"
+#echo "••••••••••••••••••••••"
 #grep -rwn 'islam' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••••"
+#echo "•••••••••••••••••••••"
 grep -rwn 'progeek' /home >> /tmp/exploits.log
-echo "••••••••••••••••••••"
+#echo "••••••••••••••••••••"
 grep -rwn 'Database Emails Extractor' /home >> /tmp/exploits.log
-echo "•••••••••••••••••••"
+#echo "•••••••••••••••••••"
 grep -rwn 'Emails Founded' /home >> /tmp/exploits.log
-echo "••••••••••••••••••"
+#echo "••••••••••••••••••"
 grep -rwn 'xVhJk6LKFv5BvQGU6mLx' /home >> /tmp/exploits.log
-echo "•••••••••••••••••"
+#echo "•••••••••••••••••"
 grep -rwn '1ZXfM6' /home >> /tmp/exploits.log
-echo "••••••••••••••••"
+#echo "••••••••••••••••"
 grep -rwn 'titik1_form' /home >> /tmp/exploits.log
-echo "•••••••••••••••"
+#echo "•••••••••••••••"
 grep -rwn 'inputzbut' /home >> /tmp/exploits.log
-echo "••••••••••••••"
+#echo "••••••••••••••"
 grep -rwn 'srpuccung' /home >> /tmp/exploits.log
-echo "•••••••••••••"
+#echo "•••••••••••••"
 grep -rwn 'oibbfucdc' /home >> /tmp/exploits.log
-echo "••••••••••••"
+#echo "••••••••••••"
 grep -rwn 'JHZpc2l0YyA9ICRfQ09PS0lFWy' /home >> /tmp/exploits.log
-echo "•••••••••••"
+#echo "•••••••••••"
 grep -rwn 'gaza.html' /home >> /tmp/exploits.log
-echo "••••••••••"
+#echo "••••••••••"
 grep -rwn 'PWD :' /home >> /tmp/exploits.log
-echo "•••••••••"
+#echo "•••••••••"
 grep -rwn 'checkPassword($passwd)' /home >> /tmp/exploits.log
-echo "••••••••"
+#echo "••••••••"
 grep -rwn 'sec-w' /home >> /tmp/exploits.log
-echo "•••••••"
+#echo "•••••••"
 grep -rwn 'Symlink_Sa' /home >> /tmp/exploits.log
-echo "••••••"
+#echo "••••••"
 grep -rwn 'Uploaded !!!' /home >> /tmp/exploits.log
-echo "•••••"
+#echo "•••••"
 grep -rwn 'Dkr9NHenNHenN' /home >> /tmp/exploits.log
-echo "••••"
+#echo "••••"
 grep -rwn 'uwPAriWb09TRe' /home >> /tmp/exploits.log
-echo "•••"
+#echo "•••"
 grep -rwn 'DamaneDz' /home >> /tmp/exploits.log
-echo "••"
+#echo "••"
 grep -rwn 'Symlink Function is DisableD' /home >> /tmp/exploits.log
-echo "•"
+#echo "•"
 grep -rwn 'JHRpbWVfc2hlbGwgPSAiIi5kYXRlKCJ' /home >> /tmp/exploits.log
-echo ""
+#echo ""
 sudo sendmail boris@boriszaydlin.com < /tmp/exploits.log
-echo "=========================="
-echo "       REPORT SENT"
-echo "=========================="
+#echo "=========================="
+#echo "       REPORT SENT"
+#echo "=========================="
 cat /tmp/exploits.log
 rm /tmp/exploits.log
 
